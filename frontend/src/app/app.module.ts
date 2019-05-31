00788c098f7059bd05145270e7e391d99ebf264b
Trinh Dinh Viet <trinh.dinh.viet@sun-asterisk.com>
2019-05-31 11:29:07 +0700

    add bootstrap frontend

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,14 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppBootstrapModule } from './app-bootstrap/app-bootstrap.module';
+import { LayoutModule} from './layouts/layout.module';
 
 @NgModule({
   declarations: [
     AppComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AppBootstrapModule,
+    LayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
